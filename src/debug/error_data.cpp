888efdf300d2345a24bdee105d07765347a6bc67
Jay Unadkat <github@unadkat.dev>
2025-01-01 01:45:44 +0000

    Licence notice update

--- src/debug/error_data.cpp
+++ src/debug/error_data.cpp
@@ -1,5 +1,6 @@
 // This file forms part of Jump (Jay's Utilities and Mathematical Primitives)
-// Copyright (C) Jay Unadkat 2024. Released under GPL-3.0-or-later (see COPYING)
+// Copyright (C) Jay Unadkat 2024--2025, released under GPL-3.0-or-later
+// See COPYING for full licence details
 
 #include "jump/debug/error_data.hpp"
 
