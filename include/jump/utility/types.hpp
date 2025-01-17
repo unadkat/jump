ebc8ce599a1abb461d4d0b820f8fcbf701150844
Jay Unadkat <github@unadkat.dev>
2025-01-17 00:36:18 +0000

    Relicense under MPL-2.0
    By agreement with all contributors (technical correctness is always the
    best kind).

--- include/jump/utility/types.hpp
+++ include/jump/utility/types.hpp
@@ -1,6 +1,8 @@
 // This file forms part of Jump (Jay's Utilities and Mathematical Primitives)
-// Copyright (C) Jay Unadkat 2024--2025, released under GPL-3.0-or-later
-// See COPYING for full licence details
+// Copyright (C) Jay Unadkat 2024--2025. SPDX-Licence-Identifier: MPL-2.0
+// This Source Code Form is subject to the terms of the Mozilla Public Licence,
+// v. 2.0. If a copy of the MPL was not distributed with this file, you can
+// obtain one at https://mozilla.org/MPL/2.0/
 
 #ifndef JUMP_TYPES_HPP
 #define JUMP_TYPES_HPP
