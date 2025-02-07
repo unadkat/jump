0f15f9cc7c22e62dbd0e028e66f8c6eaa0d2e2ed
Jay Unadkat <github@unadkat.dev>
2025-02-07 23:07:08 +0000

    Added sandbox test source for quick tests

---
+++ tests/sandbox.cpp
@@ -0,0 +1,18 @@
+// This file forms part of Jump (Jay's Utilities and Mathematical Primitives)
+// Copyright (C) Jay Unadkat 2024--2025. SPDX-Licence-Identifier: MPL-2.0
+// This Source Code Form is subject to the terms of the Mozilla Public Licence,
+// v. 2.0. If a copy of the MPL was not distributed with this file, you can
+// obtain one at https://mozilla.org/MPL/2.0/
+
+#include "jump/jump.hpp"
+
+#include <iostream>
+
+using namespace jump;
+
+int main(int argc, char** argv) {
+    CommandLineArgs args(argc, argv);
+
+    return 0;
+}
+
