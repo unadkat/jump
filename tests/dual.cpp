3bac2991dd40aa3f53e1d0bacb315f50f9bdaecd
Jay Unadkat <github@unadkat.dev>
2025-02-11 00:13:32 +0000

    Add vector expression comparisons to dual test

--- tests/dual.cpp
+++ tests/dual.cpp
@@ -9,13 +9,26 @@
 #include "jump/utility/random.hpp"
 #include "jump/testing/testing.hpp"
 
+#ifdef JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
+#include "jump/experimental/expression_templates/concepts.hpp"
+#endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
+
 #include <cmath>
 
 using namespace jump;
 
+#ifdef JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
+template <VectorExpression DualExpr, VectorExpression ValuePart,
+    VectorExpression DualPart>
+requires (is_dual_v<typename DualExpr::ValueType>)
+auto vector_compare(const DualExpr& dual_vector, const ValuePart& values,
+        const DualPart& derivatives) -> bool;
+#else
 template <std::size_t N, typename T>
 auto vector_compare(const Vector<Dual<N, T>>& dual_vector,
         const Vector<T>& values, const Vector<T>& derivatives) -> bool;
+#endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
+
 auto populate_real(Real min, Real max, std::size_t N)
         -> std::pair<Vector<d1f64>, Vector<Real>>;
 auto populate_complex(Complex begin, Complex end, std::size_t N)
@@ -38,9 +51,17 @@ int main() {
     return report.failed() > 0;
 }
 
+#ifdef JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
+template <VectorExpression DualExpr, VectorExpression ValuePart,
+    VectorExpression DualPart>
+requires (is_dual_v<typename DualExpr::ValueType>)
+auto vector_compare(const DualExpr& dual_vector, const ValuePart& values,
+        const DualPart& derivatives) -> bool {
+#else
 template <std::size_t N, typename T>
 auto vector_compare(const Vector<Dual<N, T>>& dual_vector,
         const Vector<T>& values, const Vector<T>& derivatives) -> bool {
+#endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
     if (dual_vector.size() != values.size()
             || dual_vector.size() != derivatives.size()) {
         return false;
