e14ab68ca76493fc932bf6b88218972dc4debdbb
Jay Unadkat <github@unadkat.dev>
2025-02-18 00:28:05 +0000

    Test: sandbox driver
    Includes some vector stress tests and banded matrix interface test.

--- tests/sandbox.cpp
+++ tests/sandbox.cpp
@@ -6,12 +6,94 @@
 
 #include "jump/jump.hpp"
 
+#ifdef JUMP_ENABLE_MATRIX_EXPRESSION_TEMPLATES
+#include "jump/experimental/expression_templates/concepts.hpp"
+#endif  // JUMP_ENABLE_MATRIX_EXPRESSION_TEMPLATES
+
 #include <iostream>
 
 using namespace jump;
 
+#ifdef JUMP_ENABLE_MATRIX_EXPRESSION_TEMPLATES
+template <BandedMatrixExpression Expr>
+void f(const Expr& M) {
+    std::cout << M << std::endl;
+}
+#else
+template <typename T>
+void f(const BandedMatrix<T>& M) {
+    std::cout << "No expression template support" << std::endl;
+}
+#endif  // JUMP_ENABLE_MATRIX_EXPRESSION_TEMPLATES
+
+int banded_matrix_test(int argc, char** argv);
+int vector_test(int argc, char** argv);
+
 int main(int argc, char** argv) {
     CommandLineArgs args(argc, argv);
+
+    bool do_vector_test{false};
+    bool do_banded_matrix_test{false};
+    int sum_return_vals{0};
+
+    args.get("vector", do_vector_test);
+    args.get("banded_matrix", do_banded_matrix_test);
+
+    if (do_vector_test) {
+        sum_return_vals += vector_test(argc, argv);
+    }
+    if (do_banded_matrix_test) {
+        sum_return_vals += banded_matrix_test(argc, argv);
+    }
+
+    return sum_return_vals;
+}
+
+int vector_test(int argc, char** argv) {
+    CommandLineArgs args(argc, argv);
+
+    Timer sum{"Simple sum"};
+    Timer mult_sum{"Mult sum"};
+
+    RandomReal rng_real{0., 100.};
+    std::size_t num_vectors{1000000}, vector_size{1000}, num_trials{5};
+
+    args.get("N", num_vectors);
+    args.get("size", vector_size);
+    args.get("trials", num_trials);
+
+    Vector<Vector<Real>> A(num_vectors, Vector<Real>(vector_size)),
+        B(num_vectors, Vector<Real>(vector_size)),
+        C(num_vectors, Vector<Real>(vector_size));
+    randomise(rng_real, A, B, C);
+
+    for (std::size_t i{0}; i < num_trials; ++i) {
+        sum.start();
+        for (std::size_t j{0}; j < num_vectors; ++j) {
+            A[j] = A[j] + B[j];
+        }
+        sum.stop();
+    }
+
+    for (std::size_t i{0}; i < num_trials; ++i) {
+        mult_sum.start();
+        for (std::size_t j{0}; j < num_vectors; ++j) {
+            A[j] = A[j]*(B[j] + C[j]);
+        }
+        mult_sum.stop();
+    }
+
+    std::cout << sum << std::endl;
+    std::cout << mult_sum << std::endl;
+
+    return 0;
+}
+
+int banded_matrix_test(int argc, char** argv) {
+    BandedMatrix<Real> test{10, 2};
+    RandomReal rng_real{0., 1.};
+    randomise(rng_real, test);
+    f(test);
 
     return 0;
 }
