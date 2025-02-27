7d7291dfaaaf6879c713e123fe2d03d6028304df
Jay Unadkat <github@unadkat.dev>
2025-02-27 20:01:38 +0000

    Added ConstantMatrixExpression
    Included the requirements for BandedMatrixExpressions only for now.

--- include/jump/experimental/expression_templates/constants.hpp
+++ include/jump/experimental/expression_templates/constants.hpp
@@ -9,6 +9,7 @@
 #define JUMP_EXPRESSION_TEMPLATE_CONSTANTS_HPP
 
 #include <type_traits>
+#include <utility>
 
 namespace jump {
 template <typename T>
@@ -25,11 +26,40 @@ class ConstantVectorExpression {
 
         constexpr auto operator[](std::size_t index) const -> ValueType;
         constexpr auto size() const -> std::size_t;
+
         constexpr void set_size(std::size_t N);
 
     private:
         T m_value{};
         std::size_t m_size{};
+};
+
+template <typename T>
+class ConstantMatrixExpression {
+    public:
+        using ValueType = std::remove_cvref_t<T>;
+        using InnerExpressionType = ConstantVectorExpression<ValueType>;
+
+        // Even though this is logically a "leaf" of the expression, we need to
+        // store this by value in an expression so we can update the size, etc.,
+        // proper leaves are stored by const reference which is insufficient
+        static constexpr bool is_banded_matrix_expression_leaf{false};
+
+        constexpr ConstantMatrixExpression(const T& value);
+
+        constexpr auto as_vector() const
+                -> const ConstantVectorExpression<ValueType>&;
+        constexpr auto size() const -> std::pair<std::size_t, std::size_t>;
+        constexpr auto num_elements() const -> std::size_t;
+        constexpr auto num_bands() const -> std::size_t;
+
+        constexpr void set_banded(std::size_t size, std::size_t num_bands);
+
+    private:
+        ConstantVectorExpression<ValueType> m_inner;
+        std::pair<std::size_t, std::size_t> m_size{};
+        std::size_t m_num_elements{};
+        std::size_t m_num_bands{};
 };
 
 // ========================================================================
@@ -41,13 +71,26 @@ struct IsConstantVectorExpression : public std::false_type {
 };
 
 template <typename T>
-struct IsConstantVectorExpression<ConstantVectorExpression<T>>
-        : public std::true_type {
+struct IsConstantVectorExpression<ConstantVectorExpression<T>> :
+        public std::true_type {
 };
 
 template <typename T>
 inline constexpr bool is_constant_vector_expression_v
         = IsConstantVectorExpression<T>::value;
+
+template <typename>
+struct IsConstantMatrixExpression : public std::false_type {
+};
+
+template <typename T>
+struct IsConstantMatrixExpression<ConstantMatrixExpression<T>> :
+        public std::true_type {
+};
+
+template <typename T>
+inline constexpr bool is_constant_matrix_expression_v
+        = IsConstantMatrixExpression<T>::value;
 
 // ========================================================================
 // Implementation
@@ -73,6 +116,45 @@ inline constexpr auto ConstantVectorExpression<T>::size() const -> std::size_t {
 template <typename T>
 inline constexpr void ConstantVectorExpression<T>::set_size(std::size_t N) {
     m_size = N;
+}
+
+template <typename T>
+constexpr ConstantMatrixExpression<T>::ConstantMatrixExpression(
+        const T& value) :
+    m_inner{value} {
+}
+
+template <typename T>
+inline constexpr auto ConstantMatrixExpression<T>::as_vector() const
+        -> const ConstantVectorExpression<ValueType>& {
+    return m_inner;
+}
+
+template <typename T>
+inline constexpr auto ConstantMatrixExpression<T>::size() const
+        -> std::pair<std::size_t, std::size_t> {
+    return m_size;
+}
+
+template <typename T>
+inline constexpr auto ConstantMatrixExpression<T>::num_elements() const
+        -> std::size_t {
+    return m_num_elements;
+}
+
+template <typename T>
+inline constexpr auto ConstantMatrixExpression<T>::num_bands() const
+        -> std::size_t {
+    return m_num_bands;
+}
+
+template <typename T>
+inline constexpr void ConstantMatrixExpression<T>::set_banded(std::size_t size,
+        std::size_t num_bands) {
+    m_size = {size, size};
+    m_num_bands = num_bands;
+    m_num_elements = size*(3*num_bands + 1);
+    m_inner.set_size(m_num_elements);
 }
 }   // namespace jump
 
