3432feaffca5371f35788ecd6e907ce34874ebaf
Jay Unadkat <github@unadkat.dev>
2025-04-01 23:28:25 +0100

    Use std::array for fixed-size vector storage

--- include/jump/experimental/data/vec2.hpp
+++ include/jump/experimental/data/vec2.hpp
@@ -15,87 +15,74 @@
 #include "jump/experimental/expression_templates/concepts.hpp"
 #endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
 
+#include <array>
 #include <type_traits>
 
 namespace jump {
 template <typename T>
-struct Vec<T, 2> {
+class Vec<T, 2> {
+    public:
+        using ValueType = std::remove_cvref_t<T>;
+        static constexpr bool is_vector_expression_leaf{true};
+
+        constexpr Vec(const ValueType& value = ValueType{0});
+        constexpr Vec(const ValueType& x, const ValueType& y);
+        constexpr Vec(const Vec& other) = default;
 #ifdef JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
-    using ValueType = std::remove_cvref_t<T>;
-    static constexpr bool is_vector_expression_leaf{true};
+        /// \brief Construct from a VectorExpression.
+        template <VectorExpressionConvertibleTo<ValueType> Expr>
+        constexpr Vec(const Expr& expr);
 #endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
 
-    union {T x, r, s;};
-    union {T y, g, t;};
+        constexpr auto operator[](std::size_t index) const -> const ValueType&;
+        constexpr auto operator[](std::size_t index) -> ValueType&;
 
-    constexpr Vec(const T& value = T{0});
-    constexpr Vec(const T& x, const T& y);
-#ifdef JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
-    /// \brief Construct from a VectorExpression.
-    template <VectorExpressionConvertibleTo<T> Expr>
-    constexpr Vec(const Expr& expr);
-#endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
+        constexpr auto size() const -> std::size_t;
 
-    constexpr auto operator[](std::size_t index) const -> const T&;
-    constexpr auto operator[](std::size_t index) -> T&;
 
-    constexpr auto size() const -> std::size_t;
+    private:
+        std::array<ValueType, 2> m_storage;
 };
 
 template <typename T>
-inline constexpr Vec<T, 2>::Vec(const T& value) :
-    x{value},
-    y{value} {
+inline constexpr Vec<T, 2>::Vec(const ValueType& value) :
+    m_storage{value, value} {
 }
 
 template <typename T>
-inline constexpr Vec<T, 2>::Vec(const T& x, const T& y) :
-    x{x},
-    y{y} {
+inline constexpr Vec<T, 2>::Vec(const ValueType& x, const ValueType& y) :
+    m_storage{x, y} {
 }
 
 #ifdef JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
 template <typename T>
-template <VectorExpressionConvertibleTo<T> Expr>
+template <VectorExpressionConvertibleTo<typename Vec<T, 2>::ValueType> Expr>
 inline constexpr Vec<T, 2>::Vec(const Expr& expr) :
-    x{expr[0]},
-    y{expr[1]} {
+    m_storage{expr[0], expr[1]} {
 }
 #endif  // JUMP_ENABLE_VECTOR_EXPRESSION_TEMPLATES
 
 template <typename T>
 inline constexpr auto Vec<T, 2>::operator[](std::size_t index) const
-        -> const T& {
+        -> const ValueType& {
 #ifndef NDEBUG
     if (index >= size()) {
         throw RuntimeError{Range1DError{.index = index, .size = size()}};
     }
 #endif  // NDEBUG
 
-    switch (index) {
-        default:
-        case 0:
-            return x;
-        case 1:
-            return y;
-    };
+    return m_storage[index];
 }
 
 template <typename T>
-inline constexpr auto Vec<T, 2>::operator[](std::size_t index) -> T& {
+inline constexpr auto Vec<T, 2>::operator[](std::size_t index) -> ValueType& {
 #ifndef NDEBUG
     if (index >= size()) {
         throw RuntimeError{Range1DError{.index = index, .size = size()}};
     }
 #endif  // NDEBUG
 
-    switch (index) {
-        default:
-        case 0:
-            return x;
-        case 1:
-            return y;
-    };
+    return m_storage[index];
 }
 
 template <typename T>
